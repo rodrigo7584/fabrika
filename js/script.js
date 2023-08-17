046590619bd53d18e9d75611e47bc36408903495
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-08-16 21:58:06 -0300

    16/08/2023

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 const btnMobile = document.querySelector('button.btn-mobile-menu')
+const navMobile = document.querySelector('header.menu nav')
+
 function toggleMenu() {
-  const navMobile = document.querySelector('header.menu nav')
   btnMobile.classList.toggle('active')
   navMobile.classList.toggle('active')
 }
 btnMobile.addEventListener('click', toggleMenu)
+
+const headerTopMenu = document.querySelector('header.menu')
+
+document.addEventListener('scroll', function () {
+  const scrollDistance = window.scrollY
+
+  if (scrollDistance >= 50) {
+    headerTopMenu.classList.add('scrolled')
+  } else {
+    headerTopMenu.classList.remove('scrolled')
+  }
+})
 
 const swiper = new Swiper('.swiper-hero-index', {
   direction: 'horizontal',
