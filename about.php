c3120822dc49808cfbf86210f310be1bf4b68d96
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-09-05 20:27:03 -0300

    05/09/2023

---
+++ about.php
@@ -0,0 +1,116 @@
+<?php include './components/header.php'?>
+  <main>
+    <section class="hero-about">
+      <div class="container">
+        <div class="row">
+          <div class="content">
+            <h1>Conheça mais sobre nosso trabalho no mundo infantil</h1>
+            <p>
+              Tenha um atendimento 100% personalizado, com uma equipe especializada em recreação
+              infantil
+            </p>
+            <a href="#" class="btn btn-orange">
+              <img src="./img/icon-whatsapp.png" alt="" />
+              Entre em contato
+            </a>
+          </div>
+          <div class="image">
+            <img src="./img/illustration-kids.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="sobre-nos">
+      <div class="container">
+        <div class="row">
+          <div class="image">
+            <img src="./img/group-2.png" alt="" />
+          </div>
+          <div class="content">
+            <h2>Sobre nós</h2>
+            <p>
+              Contamos com mais de 18 anos de experiência no mercado, trabalhando com recreação
+              infantil, com uma visão diferenciada e inovadora, buscando levar alegria, lazer e
+              satisfação aos nossos clientes.
+            </p>
+            <br />
+            <p>
+              Trabalhamos com todos os tipos de festas e eventos infantis - desde o aniversário
+              infantil, festas familiares como batizados, casamentos ou churrascos, eventos
+              corporativos para o público infantil até a recreação infantil em clubes, hotéis e
+              escolas.
+            </p>
+            <br />
+            <p>
+              Nossa equipe é composta por profissionais da área de educação física, pedagogia,
+              turismo e teatro.
+            </p>
+            <br />
+            <p>
+              Frequentemente, promovemos cursos e workshops para nossos profissionais para trazer
+              novas tendências e garantir muita diversão em nossos atendimentos.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mvv">
+      <img src="./img/ballon.png" alt="" class="image-ballon" />
+      <div class="container">
+        <div class="row">
+          <div class="boxes">
+            <div class="box">
+              <h2 class="title-box">Nossa Missão</h2>
+              <p>
+              Criamos momentos únicos de alegria e lazer para o nosso público, propondo
+              jogos e brincadeiras de forma divertida, para que cada um possa fazer parte
+              desta experiência inesquecível.
+              </p>
+            </div>
+            <div class="box">
+              <h2 class="title-box">Nossa Visão</h2>
+              <p>
+              Ser uma referência na área de recreação infantil e eventos para crianças e
+              adolescentes, reconhecida pela inovação na criação de serviços e produtos,
+              atendimento personalizado e resgate da interação e desenvolvimento pessoal
+              através do brincar.
+              </p>
+            </div>
+            <div class="box">
+              <h2 class="title-box">Nossos Valores</h2>
+              <ul class="red-list">
+                <li>Respeito</li>
+                <li>Diversidade</li>
+                <li>Paixão</li>
+                <li>Qualidade</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src="./img/plane.png" alt="" class="image-plane" />
+    </section>
+    <section class="numeros">
+      <div class="container">
+        <div class="row">
+          <h2>A Fabrica em números</h2>
+          <h3 class="subtitle">Aproveite nossa experiência na área de recreação infantil</h3>
+          <div class="boxes">
+            <div class="box">
+              <span>+7500</span>
+              <p>Eventos infantis realizados</p>
+            </div>
+            <div class="box">
+              <span>+14</span>
+              <p>Clubes, hotéis e empresas atentidas</p>
+            </div>
+            <div class="box">
+              <span>+1,2 Mil</span>
+              <p>Crianças felizes</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+<?php include './components/footer.php'?>
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function toggleMenu() {
 btnMobile.addEventListener('click', toggleMenu)
 
 const headerTopMenu = document.querySelector('header.menu')
-
+console.log(headerTopMenu)
 document.addEventListener('scroll', function () {
   const scrollDistance = window.scrollY
 
@@ -18,7 +18,15 @@ document.addEventListener('scroll', function () {
     headerTopMenu.classList.remove('scrolled')
   }
 })
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Script.js está sendo executado.')
 
+  // Verifique se os elementos do header estão acessíveis
+  var headerElement = document.getElementById('seu-elemento-do-header')
+  console.log(headerElement) // Verifique se isso retorna o elemento corretamente
+
+  // Seu código JavaScript que manipula elementos do header aqui
+})
 const swiperHeroIndex = new Swiper('.swiper-hero-index', {
   loop: true,
   autoplay: {
